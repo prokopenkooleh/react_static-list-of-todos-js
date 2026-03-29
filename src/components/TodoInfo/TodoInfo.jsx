ad9eba75979ebbbd1175f6c53b6b57a2376f5bd0
Oleh Prokopenko <olehprokopenko1@gmail.com>
2026-03-30 00:36:19 +0300

    find problems

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,2 +1,10 @@
+import { UserInfo } from '../UserInfo/UserInfo';
+
 // Add the required props
-export const TodoInfo = () => <>TodoInfo markup</>;
+export const TodoInfo = ({ todo, users }) => (
+  <article className="TodoInfo TodoInfo--completed">
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+
+    <UserInfo user={users} />
+  </article>
+);
